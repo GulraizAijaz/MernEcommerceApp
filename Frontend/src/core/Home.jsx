192b30a38638de96be92f8213215935eea6aaabc
GulraizAijaz <gulraizmuhammed013@gmail.com>
2025-04-26 17:33:35 +0500

    profile test

--- Frontend/src/core/Home.jsx
+++ Frontend/src/core/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => {
     <>
     <Layout title="Home Page" description="Homepage Ecommerce website "className='bg-gray-200'>
       <Search/>
-      <p className='text-3xl text-center bg-blue-300 products-wrap'>Products by Arrival {`(latest)`}</p>
+      <p className='text-3xl text-center bg-blue-300 products-wrap'>Products by Arrival {`(latest test)`}</p>
       <div className='flex justify-evenly flex-wrap '>
       {productsByArrival && productsByArrival.length > 0 ? productsByArrival.map((product,i)=>(
         <Card product={product} key={i}/>
